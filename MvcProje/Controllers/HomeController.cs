e34c301ba34496ee0c0eea925b53f68935844542
merveakca <smkirac@gmail.com>
2023-07-19 17:19:47 +0300

    Add project files.

---
+++ MvcProje/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProje.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Senin Hakkında Sayfan";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Senin İletişim Sayfan";
+
+            return View();
+        }
+        public ActionResult Bilgi()
+        {
+            return View();
+        }
+        public ActionResult Profil()
+        {
+            return View();
+        }
+    }
+}
